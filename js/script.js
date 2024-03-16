7e6731de5013f42408e8da92d13206375157d14f
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2024-03-17 00:50:11 +0600

    feedback fixed

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ const navMenu = document.querySelector('.navmenu-container');
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+new VenoBox({
+  selector: '.my-img-link',
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: 'rotating-plane',
+});
+
+new VenoBox({
+  selector: '.my-video-link',
+});
